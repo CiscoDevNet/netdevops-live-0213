627f011120dd5262437fc40cf60b60cdd23cbd58
Matt Johnson <matjohn2@cisco.com>
2019-06-25 09:18:37 +0100

    Interfaces need to be down to remove

--- z-tear-down-all-demos.sh
+++ z-tear-down-all-demos.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 # NETDEVOPS LIVE S02E13 - TEARDOWN
-# Pulls down all created interfaces and bridges.
 
+# Pulls down all created interfaces and bridges.
+for i in pair1-a pair1-b pair2-a pair2-b brtest1 brtest2 demobr1; do \
+ sudo ip link set down ${i} ; \
+done
+
+# Delete interfaces
 sudo ip link del pair1-a type veth peer name pair1-b
 sudo ip link del pair2-a type veth peer name pair2-b
 sudo ip link del brtest1
