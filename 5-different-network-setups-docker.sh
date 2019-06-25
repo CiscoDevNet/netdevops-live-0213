7b2cc9c2b452fa17b48f33fc2ce387a75c580f20
Matt Johnson <matjohn2@cisco.com>
2019-06-25 10:14:28 +0100

    host namespace test with docker

---
+++ 5-different-network-setups-docker.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# NETDEVOPS LIVE S02E13 - Example 5
+# Show docker containers using the host and no networking options.
+
+# We can tell docker not to create a networkns with --net=host.
+echo ""
+echo "Press enter to create a container using the host's default namespace..."
+read -l
+
+## Create a container with host network namespace.
+### By default ubuntu containers run bash CLI, we dont want these interactive
+### Hence the /bin/sleep and &
+sudo docker run --name container3 --network=host ubuntu:latest /bin/sleep 1000000 &
+
+
+# Output current network namespaces via lsns.
+echo "Notice below we now have THREE containers but only TWO network NS's"
+sudo docker ps
+sudo lsns --type=net
+echo "------------------"
+echo "Press enter to install ip tools CLI into container3..."
+read -l
+
+## Install some cli tools (ip) into containers 1 and 2
+## Obviously this wont survive if the container is stopped as it's not in the docker image.
+sudo docker exec -ti container3 apt update
+sudo docker exec -ti container3 apt install -y iproute2
+
+
+echo "------------------"
+echo "Press enter to run 'ip addr list' and 'ip route list' within the containers..."
+read -l
+
+echo "Container3:"
+sudo docker exec -ti container3 ip addr list
+sudo docker exec -ti container3 ip route list
